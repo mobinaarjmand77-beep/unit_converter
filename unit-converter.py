8fb72f9a1418b523cedc502256f3e9d1a36580bc
mobinaarjmand77-beep <mobinaarjmand77@gmail.com>
2026-07-01 13:59:05 +0330

    Update unit-converter.py
    ساخت كلاس پايه conveter

--- unit-converter.py
+++ unit-converter.py
@@ -1,1 +1,7 @@
-
+class Converter:
+    def __init__(self):
+        self.units_list = []
+        self.base_unit_name = ""
+    
+    def convert(self, value, from_unit, to_unit):
+        return 0
