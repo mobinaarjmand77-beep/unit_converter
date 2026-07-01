36d429fcd0684f0df9a7be3ecfec2ff813eb5fb3
mobinaarjmand77-beep <mobinaarjmand77@gmail.com>
2026-07-01 14:19:11 +0330

    Update unit-converter.py
    اضافه كردن  lengthconverter

--- unit-converter.py
+++ unit-converter.py
@@ -5,3 +5,22 @@ class Converter:
     
     def convert(self, value, from_unit, to_unit):
         return 0
+        
+class LengthConverter(Converter):
+    def __init__(self):
+        Converter.__init__(self)
+        self.base_unit_name = "meter"
+        self.rates = {
+            "meter": 1,
+            "kilometer": 1000,
+            "mile": 1609.34,
+            "foot": 0.3048,
+            "inch": 0.0254,
+            "centimeter": 0.01
+        }
+        self.units_list = list(self.rates.keys())
+    
+    def convert(self, value, from_unit, to_unit):
+        in_meter = value * self.rates[from_unit]
+        result = in_meter / self.rates[to_unit]
+        return result
